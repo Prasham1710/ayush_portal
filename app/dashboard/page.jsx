e99802f6f713cefad398d63008443d03563e7586
Prasham1710 <jainprasham17@gmail.com>
2023-09-07 01:00:38 +0530

    auth

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,5 @@
+import UserInfo from "../components/UserInfo";
+
+export default function dashboard() {
+  return <div><UserInfo/></div>;
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from 'next/image'
+import LoginForm from './components/LoginForm'
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/dashboard");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main>
+      <LoginForm />
     </main>
-  )
+  );
 }
