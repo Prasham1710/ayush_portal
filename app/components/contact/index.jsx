4caef5141aafaa3b4c0d789ce76a982c17b58256
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-09-08 20:46:16 +0530

    changes

--- app/components/contact/index.jsx
+++ app/components/contact/index.jsx
@@ -20,15 +20,15 @@ const Contact = () => {
           />
         </div>
         <form className="flex-1 flex flex-col gap-2 mr-32 ">
-          <input type="text" placeholder="name" className="p-3 bg-transparent border-4   border-[#74d292] text-[#74d292] placeholder-[#74d292] font-bold text-xl " />
-          <input type="text" placeholder="email" className="p-3 bg-transparent border-4   border-[#74d292] text-[#74d292] placeholder-[#74d292] font-bold text-xl " />
+          <input type="text" placeholder="name" className="p-3 bg-transparent border-4   border-[#74d292] text-[#74d292] placeholder-[#74d292] font-bold text-xl my-3" />
+          <input type="text" placeholder="email" className="p-3 bg-transparent border-4   border-[#74d292] text-[#74d292] placeholder-[#74d292] font-bold text-xl my-3" />
           <textarea
-            className="p-3 bg-transparent border-4   border-[#74d292] text-[#74d292] placeholder-[#74d292] font-bold text-xl"
+            className="my-3 p-3 bg-transparent border-4   border-[#74d292] text-[#74d292] placeholder-[#74d292] font-bold text-xl"
             placeholder="message"
             cols="20"
             rows="5"
           ></textarea>
-          <button  className="text-white bg-[#77d877] hover:bg-[#f8d928] text-xl px-4 py-2 font-bold rounded-lg" ><a href="#">Send</a></button>
+          <button  className="text-white bg-[#77d877] hover:bg-[#f8d928] text-xl px-4 py-2 font-bold rounded-lg my-3" ><a href="#">Send</a></button>
         </form>
       </div>
     </div>
